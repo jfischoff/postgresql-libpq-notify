3eedad691ab28a84eb12fab5f304e5619255898c
Jonathan Fischoff <jfischoff@yahoo.com>
2020-06-08 22:47:49 -0700

    Update documentation

--- src/Database/PostgreSQL/LibPQ/Notify.hs
+++ src/Database/PostgreSQL/LibPQ/Notify.hs
@@ -99,18 +99,11 @@ fdError =
 Returns a single notification.  If no notifications are
 available, 'getNotificationWithConfig' blocks until one arrives.
 Unlike 'getNotification', 'getNotificationWithConfig' takes in an
-additional 'Config' parameter which provides custom 'interrupt' and
-various event hooks for operational insight.
+additional 'Config' parameter provides event hooks for operational insight.
 
-Using a custom 'interrupt' is necessary if one would like to call
-'getNotificationWithConfig' on one thread and @NOTIFY@ on another
-thread using the same connection.
+The connection passed in cannot be used for anything else
+while waiting on the notification or this call might never return.
 
-To support this behavior one must cause 'interrupt' to return after the
-call to @NOTIFY@ checks it's result from the server.
-
-See the test file of this package for an example of how to use a custom
-'interrupt'.
 
 Note that PostgreSQL does not
 deliver notifications while a connection is inside a transaction.
@@ -152,11 +145,8 @@ getNotificationWithConfig Config {..} c = fmap (first setLoc) $ try $ fix $ \nex
 Returns a single notification.  If no notifications are
 available, 'getNotification' blocks until one arrives.
 
-If 'getNotification' is called and afterwards on a different thread
-@NOTIFY@ is called using the same connection, 'getNotification' can
-block even if a notification is sent.
-
-To support this behavior one must use 'getNotificationWithConfig' instead.
+The connection passed in cannot be used for anything else
+while waiting on the notification or this call might never return.
 
 Note that PostgreSQL does not
 deliver notifications while a connection is inside a transaction.
