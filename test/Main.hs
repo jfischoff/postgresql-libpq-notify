76ec46bf574095ece57f348c226ec5f747f7aa67
Jonathan Fischoff <jfischoff@yahoo.com>
2020-06-08 22:37:01 -0700

    Simplify interface and implementation to prevent the race condition #6

--- test/Main.hs
+++ test/Main.hs
@@ -38,46 +38,45 @@ aroundAll withFunc specWith = do
 
   beforeAll theStart $ afterAll theStop $ specWith
 
-withSetup :: (PQ.Connection -> IO ()) -> IO ()
+withSetup :: ((PQ.Connection, PQ.Connection) -> IO ()) -> IO ()
 withSetup f = either throwIO pure <=< Temp.withDbCache $ \dbCache ->
   Temp.withConfig (Temp.defaultConfig <> Temp.cacheConfig dbCache) $ \db -> do
     let localhostOpts = Temp.toConnectionOptions db
     bracket (PQ.connectdb (Options.toConnectionString localhostOpts))
       PQ.finish
-      f
+      $ \c -> bracket (PQ.connectdb (Options.toConnectionString localhostOpts))
+          PQ.finish
+          $ \c1 -> f (c, c1)
 
 spec :: Spec
 spec = aroundAll withSetup $ do
   describe "getNotificationWithConfig" $ describe "returns a notification" $ do
-    it "before" $ \conn -> do
-      connVar <- newMVar conn
+    it "before" $ \(conn1, conn2) -> do
       let initialChannel = "channel"
           initialData = "hi!"
-      _ <- withMVar connVar $ \c -> do
-          _ <- PQ.exec c $ "LISTEN " <> initialChannel
-          PQ.exec c ("NOTIFY " <> initialChannel <> ", '" <> initialData <>"';")
 
-      Right PQ.Notify {..} <- getNotification withMVar connVar
+      _ <- PQ.exec conn2 $ "LISTEN " <> initialChannel
+      _ <- PQ.exec conn1 $ "NOTIFY " <> initialChannel <> ", '" <> initialData <>"';"
+
+      Right PQ.Notify {..} <- getNotification conn2
       notifyRelname `shouldBe` initialChannel
       notifyExtra `shouldBe` initialData
 
-    it "after file notifications are registered" $ \conn -> do
-      connVar <- newMVar conn
+    it "after file notifications are registered" $ \(conn1, conn2) -> do
       ender <- newEmptyMVar
       beforeWaiter <- newEmptyMVar
       let initialChannel = "channel"
           initialData = "hi!"
-      _ <- withMVar connVar $ \c -> PQ.exec c $ "LISTEN " <> initialChannel
+      _ <- PQ.exec conn2 $ "LISTEN " <> initialChannel
       _ <- forkIO $ do
              takeMVar beforeWaiter
-             _ <- withMVar connVar $ \c ->  PQ.exec c ("NOTIFY " <> initialChannel <> ", '" <> initialData <>"';")
+             _ <- PQ.exec conn1 ("NOTIFY " <> initialChannel <> ", '" <> initialData <>"';")
              putMVar ender ()
 
       let config = defaultConfig
-            { interrupt  = Just $ takeMVar ender
-            , beforeWait = putMVar beforeWaiter ()
+            { beforeWait = putMVar beforeWaiter ()
             }
 
-      Right PQ.Notify {..} <- getNotificationWithConfig config withMVar connVar
+      Right PQ.Notify {..} <- getNotificationWithConfig config conn2
       notifyRelname `shouldBe` initialChannel
       notifyExtra `shouldBe` initialData
